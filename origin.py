5c29b9c7f92bb1e9b45862451895ba7fa9eb6a74
Logan Turner <lturnerbio@gmail.com>
2021-04-26 13:05:46 -0500

    updated script to parse origin.txt

--- origin.py
+++ origin.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-import re 
+import re
 
 """
 This code should work to parse through a text file (in this case the origin of species)
@@ -14,4 +14,20 @@ Let's see how this goes
 #if  __name__ == '__main__'
 
 parse_pat = "\w*herit\w*"
+# I believe that this pattern should return everything relating to heritability
+parse_obj = re.compile(parse_pat, re.IGNORECASE)
 
+
+with open('origin.txt', 'r') as in_stream:
+    print('Origin.txt file opened')
+    with open('herit_occurences.txt', 'w') as out_stream:
+        for line_index, line in enumerate(in_stream, 1):
+            if parse_obj.findall(line):
+                match = parse_obj.search(line)
+                out_stream.write("{0}\t{1}\n".format(line_index, match.group()))
+
+print('Done!')
+print('dummy.txt is closed?', in_stream.closed)
+print('output.txt is closed?', out_stream.closed)
+
+print('output file should be labelled herit_occurences.txt')
