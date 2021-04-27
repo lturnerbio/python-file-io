32f2eb004c61c69eb2ad85a6aa7d6467dd7988d3
Logan Turner <lturnerbio@gmail.com>
2021-04-27 09:48:54 -0500

    added if name == main

--- origin.py
+++ origin.py
@@ -11,23 +11,24 @@ and return words that relate to heritability. The script should return the line 
 Let's see how this goes
 """
 
-#if  __name__ == '__main__'
-
-parse_pat = "\w*herit\w*"
-# I believe that this pattern should return everything relating to heritability
-parse_obj = re.compile(parse_pat, re.IGNORECASE)
+if  __name__ == '__main__':
 
 
-with open('origin.txt', 'r') as in_stream:
-    print('Origin.txt file opened')
-    with open('herit_occurences.txt', 'w') as out_stream:
-        for line_index, line in enumerate(in_stream, 1):
-            if parse_obj.findall(line):
-                match = parse_obj.search(line)
-                out_stream.write("{0}\t{1}\n".format(line_index, match.group()))
+    parse_pat = "\w*herit\w*"
+    # I believe that this pattern should return everything relating to heritability
+    parse_obj = re.compile(parse_pat, re.IGNORECASE)
 
-print('Done!')
-print('dummy.txt is closed?', in_stream.closed)
-print('output.txt is closed?', out_stream.closed)
 
-print('output file should be labelled herit_occurences.txt')
+    with open('origin.txt', 'r') as in_stream:
+        print('Origin.txt file opened')
+        with open('herit_occurences.txt', 'w') as out_stream:
+            for line_index, line in enumerate(in_stream, 1):
+                if parse_obj.findall(line):
+                    match = parse_obj.search(line)
+                    out_stream.write("{0}\t{1}\n".format(line_index, match.group()))
+
+    print('Done!')
+    print('dummy.txt is closed?', in_stream.closed)
+    print('output.txt is closed?', out_stream.closed)
+
+    print('output file should be labelled herit_occurences.txt')
